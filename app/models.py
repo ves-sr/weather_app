229256eff4ad7f9b13f52f0384c9c005b5c39fc1
ves-sr <veswerk@gmail.com>
2026-07-30 22:50:39 +0900

    feat: User・UserSettingのDB設計を追加

---
+++ app/models.py
@@ -0,0 +1,35 @@
+from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+
+engine = create_engine("sqlite:///weather_app.db")
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String, unique=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    line_user_id = Column(String, unique=True, nullable=True)
+    
+
+    setting = relationship("UserSetting", back_populates="user", uselist=False)
+
+
+class UserSetting(Base):
+    __tablename__ = "user_settings"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer,  ForeignKey("users.id"), nullable=False)
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
+    notify_hour = Column(Integer, nullable=False)
+    notify_minute = Column(Integer, nullable=False)
+    rain_threshold = Column(Float, nullable=False)
+
+    user = relationship("User", back_populates="setting")
+
+
+Base.metadata.create_all(bind=engine)
